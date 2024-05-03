23a49afa1276ae24f99fdb3e5a9cc47c5ee8587f
Your Name <you@example.com>
2024-05-03 09:56:51 -0300

    se realizo el trabajo del archivo rangoEdad.js

---
+++ assets/js/rangoEdad.js
@@ -0,0 +1,12 @@
+let edad = parseInt(prompt("Ingrese su edad"));
+
+
+if(edad = 0 && edad == 12){
+    console.log("es un niño")
+}else if(edad = 13 && edad ==19){
+    console.log("es un adolescente")
+}else if(edad = 20 && edad == 59){
+    console.log("es un adulto")
+}else if(edad > 60){
+    console.log("es un adulto mayor")
+}

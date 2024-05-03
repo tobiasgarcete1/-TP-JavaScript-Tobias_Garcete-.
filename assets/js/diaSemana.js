b19fb558fc7373e6672190d1edf798dd9a9c0384
Your Name <you@example.com>
2024-05-03 10:23:40 -0300

    se realizo el trabajo del archivo diaSemana.js

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,27 @@
+let dia = parseInt(prompt("Ingrese un dia de la semana"));
+
+switch (dia){
+    case 1:
+        console.log("lunes");
+        break
+    case 2:
+        console.log("Martes");
+        break
+    case 3:
+        console.log("Miercoles");
+        break
+    case 4:
+        console.log("Jueves");
+        break
+    case 5:
+        console.log("Viernes");
+        break
+    case 6: 
+    console.log("Sabado");
+        break
+    case 7:
+        console.log("Domingo");
+        break
+    default:
+        console.log("Ingrese un numero valido")    
+}

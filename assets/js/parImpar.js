6897fa071413742dea44be69def742db8a5c6653
Your Name <you@example.com>
2024-05-03 10:14:25 -0300

    se realizo el trabajo del archivo parImpar.js

--- assets/js/parImpar.js
+++ assets/js/parImpar.js
@@ -1,7 +1,9 @@
 let num =parseInt(prompt("Ingrese un numero "));
 
-if(num / 2 === 0){
+if(num % 2 === 0){
     console.log("El numero ingresado es par")
-}else if(num / 2 !== 0){
+}else if(num % 2 !== 0){
     console.log("El  numero ingresado no es par")
+}else{
+    console.log("Ingrese un valor valido")
 }
